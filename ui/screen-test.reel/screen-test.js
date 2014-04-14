030ca159be6520d464a89afa826f0b3b7233c9ee
Michael Koch <noreply>
2014-04-14 21:36:02 +0000

    Add component screen-test

---
+++ ui/screen-test.reel/screen-test.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/screen-test.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class ScreenTest
+ * @extends Component
+ */
+exports.ScreenTest = Component.specialize(/** @lends ScreenTest# */ {
+    constructor: {
+        value: function ScreenTest() {
+            this.super();
+        }
+    }
+});
